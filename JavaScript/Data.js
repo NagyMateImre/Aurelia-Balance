5cae243ca830ca61f6e656af47fb598be087beef
NagyMateImre <nagym3@kkszi.hu>
2025-12-18 21:47:55 +0100

    90% done

---
+++ JavaScript/Data.js
@@ -0,0 +1,105 @@
+let Termekek = [
+    {
+        TermekNeve: "Súlyozott takaró (Weighted Blanket)",
+        TermekAra: 24900,
+    },
+    {
+        TermekNeve: "Kékfény-szűrős szemüveg",
+        TermekAra: 6500,
+    },
+    {
+        TermekNeve: "Fehér zaj gép (White Noise Machine)",
+        TermekAra: 12500,
+    },
+    {
+        TermekNeve: "Kontúros alvómaszk",
+        TermekAra: 3900,
+    },
+    {
+        TermekNeve: "Ergonomikus térdpárna",
+        TermekAra: 7500,
+    },
+    {
+        TermekNeve: "Alvást segítő ajakragasztó (Mouth Tape)",
+        TermekAra: 4200,
+    },
+    {
+        TermekNeve: "Aromaterápiás diffúzor és illóolaj szett",
+        TermekAra: 14900,
+    },
+    {
+        TermekNeve: "Hideg gőz párásító",
+        TermekAra: 18500,
+    },
+    {
+        TermekNeve: "Stresszoldó Fidget játék csomag",
+        TermekAra: 3500,
+    },
+    {
+        TermekNeve: "Strukturált Hála napló",
+        TermekAra: 5900,
+    },
+    {
+        TermekNeve: "Professzionális masszázspisztoly",
+        TermekAra: 29900,
+    },
+    {
+        TermekNeve: "Okos sportkarkötő (Fitness Tracker)",
+        TermekAra: 15900,
+    },
+    {
+        TermekNeve: "Vibrációs fascia henger",
+        TermekAra: 11500,
+    },
+    {
+        TermekNeve: "Kompressziós zokni (pár)",
+        TermekAra: 5500,
+    },
+    {
+        TermekNeve: "Prémium protein rázó (Shaker Bottle)",
+        TermekAra: 3200,
+    },
+    {
+        TermekNeve: "Csúszásmentes jóga szőnyeg",
+        TermekAra: 8900,
+    },
+    {
+        TermekNeve: "Magnézium-biszglicinát kapszula",
+        TermekAra: 5800,
+    },
+    {
+        TermekNeve: "Ashwagandha kivonat (KSM-66)",
+        TermekAra: 4900,
+    },
+    {
+        TermekNeve: "Oroszlánsörény gomba por",
+        TermekAra: 7200,
+    },
+    {
+        TermekNeve: "Komplex Probiotikum (30 adag)",
+        TermekAra: 6500,
+    },
+    {
+        TermekNeve: "D3-vitamin szájspray",
+        TermekAra: 3800,
+    },
+    {
+        TermekNeve: "Kézműves fürdőbomba válogatás",
+        TermekAra: 4500,
+    },
+    {
+        TermekNeve: "Gua Sha kő és Jade Roller készlet",
+        TermekAra: 6900,
+    },
+    {
+        TermekNeve: "Súlyozott, melegíthető szemmaszk",
+        TermekAra: 8200,
+    },
+    {
+        TermekNeve: "Zero Waste szilárd sampon",
+        TermekAra: 3400,
+    }
+];
+ 
+
+localStorage.setItem("Termekek",JSON.stringify(Termekek))
---
+++ JavaScript/Products.js
@@ -0,0 +1,138 @@
+const search = document.getElementById('search');
+
+const local_kapott_alap = localStorage.getItem("Termekek");
+const local_alakitott_alap = JSON.parse(local_kapott_alap);
+
+let Termek = []
+
+const local_kapott = localStorage.getItem("TermekekUj");
+const local_alakitott = JSON.parse(local_kapott);
+
+for(let index in local_alakitott_alap){
+    Termek[index] = local_alakitott_alap[index];
+}
+
+
+for(let index in local_alakitott){
+    Termek[index] = local_alakitott[index];
+}
+
+console.log(Termek)
+
+const Submit_btn = document.getElementById('Submit_btn');
+const Add_btn = document.getElementById('Add_btn');
+
+document.getElementById('NewProduct').style.display = "none"
+document.getElementById('Product_Update').style.display = "none"
+
+function Add_Products(){
+    document.getElementById('NewProduct').style.display = "";
+}
+
+function Close_Btn(){
+    document.getElementById('NewProduct').style.display = "none"
+    window.location.reload();
+}
+
+function Submit_Btn(){
+    const NewProducts = {
+        TermekNeve: document.getElementById('Product_Name').value,
+        TermekAra: Number(document.getElementById('Product_Price').value)
+    }
+
+    Termek.push(NewProducts)
+    localStorage.setItem("TermekekUj",JSON.stringify(Termek))
+
+    document.getElementById('NewProduct').style.display = "none"
+    window.location.reload();
+}
+
+let TorlesElem = 0;
+let New_tomb = []
+function Delete_btn(){
+    TorlesElem = event.target.id;
+
+    for(let index in Termek){
+        if(index != TorlesElem){
+            New_tomb.push(Termek[index])
+        }
+    }
+
+    Termek = []
+
+    for(let index in New_tomb){
+        Termek[index] = New_tomb[index];
+    }
+
+    localStorage.clear();
+    localStorage.setItem("TermekekUj",JSON.stringify(Termek));
+    window.location.reload();
+}
+
+const Cards = document.getElementById('Cards');
+
+let IndexedikElem = 0;
+
+// modositas gomb
+
+function Update_Btn(){
+    Termek[IndexedikElem].TermekNeve = document.getElementById('Product_Current_Name').value;
+    Termek[IndexedikElem].TermekAra = Number(document.getElementById('Product_Current_Price').value);
+    localStorage.setItem("TermekekUj",JSON.stringify(Termek))
+    window.location.reload();
+}
+
+// Menu Gomb
+
+function Update_btn(){
+    document.getElementById('Product_Update').style.display = "";
+    document.getElementById('Product_Current_Name').value = Termek[event.target.id].TermekNeve;
+    document.getElementById('Product_Current_Price').value = Termek[event.target.id].TermekAra;
+    IndexedikElem = event.target.id;
+}
+
+document.addEventListener('DOMContentLoaded', e =>{
+    e.preventDefault()
+
+    for(let index in Termek){
+        const Card_div = document.createElement('div');
+        Card_div.setAttribute("class","card_main");
+
+        const Card_img = document.createElement('img');
+        Card_img.setAttribute("class","card_img");
+        Card_img.setAttribute("src", `${index}`)
+        Card_img.src = `../IMG/Termek1.png`;
+
+        const Card_Info_div = document.createElement('div');
+        Card_Info_div.setAttribute("class","card_description");
+
+        const Card_h1 = document.createElement('h1');
+        Card_h1.setAttribute("class","card_products_name");
+        Card_h1.textContent = Termek[index].TermekNeve;
+
+        const Card_a = document.createElement('a');
+        Card_a.setAttribute("class","card_products_price");
+        Card_a.textContent = Termek[index].TermekAra;
+
+        const Card_Button = document.createElement('button');
+        Card_Button.setAttribute("class","Delete_btn");
+        Card_Button.textContent = "Termék Törlése";
+        Card_Button.setAttribute("id",index);
+        Card_Button.setAttribute('onclick',"Delete_btn()")
+
+        const Update_button = document.createElement('button');
+        Update_button.setAttribute("class","Update_btn");
+        Update_button.textContent = "Termék módosítása";
+        Update_button.setAttribute("id",index);
+        Update_button.setAttribute('onclick',"Update_btn()");
+
+
+        Cards.appendChild(Card_div);
+        Card_div.appendChild(Card_img);
+        Card_div.appendChild(Card_Info_div);
+        Card_Info_div.appendChild(Card_h1);
+        Card_Info_div.appendChild(Card_a);
+        Card_Info_div.appendChild(Card_Button);
+        Card_Info_div.appendChild(Update_button)
+    }
+})
